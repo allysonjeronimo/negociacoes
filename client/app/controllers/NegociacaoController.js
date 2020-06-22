490291695df9a3bb66fa12b042fcdffae340a183
Allyson <allysonjeronimo@gmail.com>
2020-06-22 17:53:42 -0300

    Parse da data no controller

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,6 +1,6 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    constructor(){
+    constructor() {
         let $ = document.querySelector.bind(document)
 
         this._inputData = $('#data')
@@ -8,13 +8,28 @@ class NegociacaoController{
         this._inputValor = $('#valor')
     }
 
-    adiciona(event){
+    adiciona(event) {
         event.preventDefault()
 
-        console.log(
-            this._inputData.value, 
-            this._inputQuantidade.value, 
-            this._inputValor.value)
+        //let data = new Date(this._inputData.value.split('-'))
+        //let data = new Date(this._inputData.value.replace(/-/g, ','))
+        // mes = 0..11
+        // index == 1 ? item-1 : item
+        // return item - index % 2
+        let data = new Date(
+            ...
+            this._inputData.value.split('-')
+            .map((item, index) => index - index % 2))
+
+        let quantidade = this._inputQuantidade.value
+        let valor = this._inputValor.value
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(quantidade),
+            parseFloat(valor))
+
+        console.log(negociacao, data, quantidade, valor)
 
         alert('NegociacaoController: adiciona()')
     }
